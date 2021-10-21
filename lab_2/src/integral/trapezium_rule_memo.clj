55698c6330206def793a142f32d645d696636e3c
Tigran Manasyan <manasyan_tigran@mail.ru>
2021-10-21 22:18:34 +0700

    removed global sequence from 2.1

--- lab_2/src/integral/trapezium_rule_memo.clj
+++ lab_2/src/integral/trapezium_rule_memo.clj
@@ -1,16 +1,16 @@
 ; 2.1
 (ns integral.trapezium-rule-memo)
 
-(def area-memo (memoize (fn area [f x_i step]
-                          (if (not= 0 x_i)
-                            (+ (area-memo f (- x_i step) step)
-                               (* (/ (+ (f x_i)
-                                        (f (- x_i step)))
-                                     2)
-                                  step))
-                            0.0))))
-
 (defn integral-memo [f step]
-  #(area-memo f
-              (* step (quot % step))
-              step))
+  (let [area (memoize (fn [rec-fn f x_i step]
+                        (if (not= 0 x_i)
+                          (+ (rec-fn rec-fn f (- x_i step) step)
+                             (* (/ (+ (f x_i)
+                                      (f (- x_i step)))
+                                   2)
+                                step))
+                          0.0)))
+        area-memo (partial area area)]
+    #(area-memo f
+                (* step (quot % step))
+                step)))
